6cf557e4749a6fbfdeff187d6b35a0ae54ad3bf2
Luiz Carlos <frickajr@gmail.com>
2021-11-22 19:17:48 -0300

    início do projeto

---
+++ Assets/MovimentoBola.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovimentoBola : MonoBehaviour
+{
+    Rigidbody rb;
+    public float velocidade = 10;
+    public GameObject objeto;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        rb.AddTorque(Input.GetAxis("Vertical") * velocidade,
+                     0,
+                     Input.GetAxis("Horizontal") * -velocidade);
+#endif
+#if UNITY_ANDROID
+        rb.AddTorque(Input.acceleration.y * velocidade,
+                     0,
+                     Input.acceleration.x * -velocidade);
+#endif
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+            Instantiate(objeto, transform);
+    }
+}
